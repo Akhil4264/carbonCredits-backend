d5b7d86963a581ded446d7ecfd07aacd7e41c1a1
akhil dekarla <akhil.dekarla@hexagon.com>
2025-04-20 14:11:58 +0530

    changes to cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://carboncredits-frontend-1.onrender.com/',
+  origin: 'https://carboncredits-frontend-1.onrender.com',
   credentials: true,
 }));
 
